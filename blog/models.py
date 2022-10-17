16ba45374cffbb46790336de7011088010d2b5a2
Anjo Tadena <tadena.anjo@gmail.com>
2022-10-17 20:01:10 +0800

    Create Post and Comment model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.urls import reverse
+from django.utils import timezone
 
 # Create your models here.
+class Post(models.Model):
+    # Foreign key for a authenticated user relationship
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now())
+    published_at = models.DateTimeField(blank=True, null=True)
+    
+    def publish(self):
+        self.published_at = timezone.now()
+        self.save()
+        
+    def approve_commets(self):
+        return self.comments.filter(approved_comments=True)
+    
+    def get_absolute_url(self):
+        return reverse("pos_detail", kwargs={"pk": self.pk})
+    
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', related_name="comments")
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now())
+    approved_comment = models.BooleanField(default=False)
+    
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+        
+    def get_absolute_url(self):
+        return reverse("post_list")
+    
+
+    def __str__(self):
+        return self.text
